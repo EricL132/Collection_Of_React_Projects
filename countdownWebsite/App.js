73156e4ae42f5fb8dc78ffa83a1a9344c786a268
Eric <ericliao132@gmail.com>
2022-01-21 19:02:44 -0500

    added comments to countdownWebsite

--- countdownWebsite/App.js
+++ countdownWebsite/App.js
@@ -1,65 +1,74 @@
-import './App.css';
-import React from 'react'
-let interval
+import "./App.css";
+import React from "react";
+let interval;
 class App extends React.Component {
   constructor() {
-    super()
-    this.state = {finalsWeek:false}
-    this.countDown = this.countDown.bind(this)
+    super();
+    this.state = { loaded: false };
+    this.countDown = this.countDown.bind(this);
   }
-
+  
   countDown() {
-    this.setState({finalsWeek:true})
-    const newYearsDate = new Date('1 1 2022')
-    const currentDate = new Date()
+    this.setState({ loaded: true });
+    //Gets date and current date to find time remaining
+    const newYearsDate = new Date(`1 1 ${new Date().getFullYear()+1}`);
+    const currentDate = new Date();
+
+    //Get time remaining for each interval (seconds, days, hours, mintues, seconds)
     const seconds = (newYearsDate - currentDate) / 1000;
-    const days = Math.floor(seconds / 3600 / 24)
+    const days = Math.floor(seconds / 3600 / 24);
     const hours = Math.floor(seconds / 3600 / 24) % 24;
     const minutes = Math.floor(seconds / 3600 / 24) % 60;
     const sec = Math.floor(seconds % 60);
-    document.getElementById('Days').innerHTML = days
-    document.getElementById('hours').innerHTML = hours
-    document.getElementById('minutes').innerHTML = minutes
-    document.getElementById('seconds').innerHTML = sec
-    
+
+    //Sets element text equal to time of each interval
+    document.getElementById("Days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = sec;
   }
+
+  //Runs on load sets interval to run countdown function every 1 second
   componentDidMount() {
     interval = setInterval(() => {
       this.countDown();
-    }, 1000)
+    }, 1000);
   }
+
+  //Clears interval when site is closed 
   componentWillUnmount() {
-    clearInterval(interval)
+    clearInterval(interval);
   }
   render() {
     return (
       <>
-
-    {this.state.finalsWeek?
-      <div className="countdown-full">
-      <h1>New Years</h1>
-      <h2>January 1st</h2>
-      <div className="countdown-container">
-        <div className="count-dm days-c">
-          <p className="big-text" id="Days"></p>
-          <span>Days</span>
-        </div>
-        <div className="count-dm hours-c">
-          <p className="big-text" id="hours"></p>
-          <span>Hours</span>
-        </div>
-        <div className='count-dm minutes-c'>
-          <p className='big-text' id='minutes'></p>
-          <span>Minutes</span>
-        </div>
-        <div className='count-dm seconds-c'>
-          <p className='big-text' id='seconds'></p>
-          <span>Seconds</span>
-        </div>
-      </div>
-      </div>
-     : <div className="countdown-full"></div> }
-     </>
+        {this.state.loaded ? (
+          <div className="countdown_full">
+            <h1>New Years</h1>
+            <h2>January 1st</h2>
+            <div className="countdown_container">
+              <div className="count_dm days_c">
+                <p className="big_text" id="Days"></p>
+                <span className="countdm_normal_span">Days</span>
+              </div>
+              <div className="count_dm hours_c">
+                <p className="big_text" id="hours"></p>
+                <span className="countdm_normal_span">Hours</span>
+              </div>
+              <div className="count_dm minutes_c">
+                <p className="big_text" id="minutes"></p>
+                <span className="countdm_normal_span">Minutes</span>
+              </div>
+              <div className="count_dm seconds_c">
+                <p className="big_text" id="seconds"></p>
+                <span className="countdm_normal_span">Seconds</span>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="countdown_full">Loading...</div>
+        )}
+      </>
     );
   }
 }
